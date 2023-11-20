3700ea2f0e941cd7648c484dfdee2924b45ad25f
Git-MuramatsuT <t.muramatsu0485@gmail.com>
2023-11-20 10:04:24 +0900

    ui追加

---
+++ beginner/ui/index.js
@@ -0,0 +1,21 @@
+// Javascriptからのメッセージを表示
+function helloFromJavascript() {
+    alert("hello from javascript");
+}
+
+// Java(サーバー)からのメッセージを表示
+async function helloFromJava() {
+    // fetchにより、指定したエンドポイントとhttp通信する
+    const response = await fetch("http://localhost:8080/hello");
+    if (!response.ok) {
+        alert("something wrong! have you run the server yet?");
+        return;
+    }
+    const text = await response.text();
+    alert(text);
+}
+
+// TODO: helloExercise()を完成させて、サーバーからのメッセージをアラートで表示する
+async function helloExercise() {
+    alert("I'm not getting a message!");
+}
