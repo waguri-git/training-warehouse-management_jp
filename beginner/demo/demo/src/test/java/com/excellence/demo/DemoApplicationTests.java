8262a6e86b88e79693ef1fc7e86f7908bdb126fe
Git-MuramatsuT <120155630+Git-MuramatsuT@users.noreply.github.com>
2023-11-20 09:31:45 +0900

    Add files via upload

---
+++ beginner/demo/demo/src/test/java/com/excellence/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
