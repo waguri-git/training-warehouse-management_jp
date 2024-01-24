b006c61fc74a70d0fa10cd93d41e0d82fca818c6
Git-Nonaka <nonaka-mei@outlook.jp>
2024-01-24 14:02:57 +0900

    fetch、if

--- beginner/demo/demo/src/main/java/com/excellence/demo/controller/HelloController.java
+++ beginner/demo/demo/src/main/java/com/excellence/demo/controller/HelloController.java
@@ -15,3 +15,13 @@ public class HelloController {
         return "Greetings from Java Spring Boot!";
     }
 }
+
+
+//@RequestMappingは()内の住所を設定している(リクエスト先を設定)
+//@GetMappingの場合14行目の回答が返る
+//Mappingは目的地の設定、ピン刺しの意味
+//Getリクエストが/helloにMappingされ、GetMappingで振り分けられる
+//GETやPUTはリクエストメソッドの種類（補助資料参考）
+//9～10行目のアノテーションはクラスについているが、12～13行目のアノテーションはクラス内に存在し、14行目についている
+//404NotFoundedはHTTPレスポンスのエラーコード
+//13行目の()内でHTTPレスポンスのステータス行の3桁の数字を設定している（OKは200）
